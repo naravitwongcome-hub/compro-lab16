495685a64857f47328eea4d4f075e3a6954a0594
GAME <naravitwongcome@gmail.com>
2026-02-06 11:42:13 +0700

    lab16_1_complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,36 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a,int N , int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a , int N , int M){
+    for(int i = 0; i < N*M; i++){
+        *(a+i)= (rand()%101)/100.0;
+    }
+}
+
+void findRowSum(const double *a, double *s, int N, int M) {
+    for (int i = 0; i < N; i++) {
+        s[i] = 0;
+        for (int j = 0; j < M; j++) {
+            s[i] += *(a + (i * M + j)); 
+        }
+    }
+}
+
+void findColSum(const double *a, double *s, int N, int M) {
+    for (int j = 0; j < M; j++) {
+        s[j] = 0; // 
+        for (int i = 0; i < N; i++) {
+            s[j] += a[i * M + j]; 
+        }
+    }
+}
